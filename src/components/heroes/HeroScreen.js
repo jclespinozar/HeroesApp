e98770221e90ebeb73fa5969ce2284e227cf7e62
Jose Espinoza <jespinozar@uni.pe>
2022-04-04 16:43:08 -0500

    heroe screen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,39 +1,69 @@
-import { useParams , Navigate} from "react-router-dom"
+import { useParams, Navigate } from "react-router-dom";
 import { getHeroesById } from "../../selectors/getHeroesById";
+import { useEffect, useState } from "react";
 
-export const HeroScreen = ({history}) => {
-  const {heroId} = useParams();
-  const [hero] = getHeroesById(heroId);
-  
+export const HeroScreen = ({ history }) => {
+  const { heroId } = useParams();
+  const [hero, setHero] = useState({});
+
+  useEffect(() => {
+    getHeroesById(heroId).then((hero) => {
+      console.log(hero);
+      setHero(hero);
+    });
+  }, []);
+
   const handleReturn = () => {
     console.log(history);
-  }
+  };
 
-  if (hero.length==0) {
-    return (
-      <Navigate replace to="/" />
-    )
-  }
   return (
-    <div> 
-      <div className="row mt-5">
-        <div className="col-4">
-          <img src={hero.thumbnail.path+'.'+hero.thumbnail.extension} className="img-thumbnail" />
+    <div>
+      {hero != {} && (
+        <div className="row mt-5">
+          <div className="col-4">
+            <img
+              src={hero.thumbnail.path + "." + hero.thumbnail.extension}
+              className="img-thumbnail"
+            />
+          </div>
+          <div className="col-8">
+            <h3>{hero.name}</h3>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <b>Historia</b> {hero.description}
+              </li>
+              <li className="list-group-item">
+                <b>Comics</b>
+                {hero.comics.items.map((comic) => (
+                  <p>{comic.name}</p>
+                ))}
+              </li>
+              <li className="list-group-item">
+                <b>Series</b>
+                {hero.series.items.map((serie) => (
+                  <p>{serie.name}</p>
+                ))}
+              </li>
+              <li className="list-group-item">
+                <b>Series</b>
+                {hero.stories.items.map((story) => (
+                  <p>{story.name}</p>
+                ))}
+              </li>
+              <li className="list-group-item">
+                <b>Eventos</b>
+                {hero.events.items.map((event) => (
+                  <p>{event.name}</p>
+                ))}
+              </li>
+            </ul>
+            <button className="btn btn-outline-info" onClick={handleReturn}>
+              Return
+            </button>
+          </div>
         </div>
-        <div className="col-8">
-          <h3>{hero.name}</h3>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item"><b>Alter ego</b> ejemplo</li>
-            <li className="list-group-item"><b>Publisher</b> ejemplo</li>
-            <li className="list-group-item"><b>First appearance</b> ejemplo</li>
-          </ul>
-          
-          <h5> Characters</h5>
-          <p>ejemplo</p>
-
-          <button className="btn btn-outline-info" onClick={handleReturn}>Return</button>
-        </div>
-      </div>
+      )}
     </div>
-  )
-}
+  );
+};
--- src/selectors/getHeroesById.js
+++ src/selectors/getHeroesById.js
@@ -1,5 +1,10 @@
-import { heroes } from "../data/heroes";
+import { useEffect, useState } from "react";
 
-export const getHeroesById = function (id) {
-  return heroes.filter( hero => hero.id == id);
-}
+export const getHeroesById = async (id) => {
+  const response = await fetch(
+    `http://gateway.marvel.com/v1/public/characters/${id}?apikey=31a576420eac3d37aedaa8e207b6ce17&ts=1000&hash=4ba076bb06fbf679a4eac2072a674de1`
+  );
+  const data = await response.json();
+
+  return data.data.results[0];
+};
